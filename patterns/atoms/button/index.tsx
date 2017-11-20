b450562368043509f26a194435f3e07703b9e846
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-11-20 19:05:17 +0100

    add color to button

--- patterns/atoms/button/index.tsx
+++ patterns/atoms/button/index.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import styled, { css } from 'styled-components';
+import colors from 'colors';
 
 const StyledButton = styled.button`
-	background: red;
+	background: ${colors.greenDark};
 `;
 
 const Button: React.StatelessComponent<{}> = (props): JSX.Element => {
