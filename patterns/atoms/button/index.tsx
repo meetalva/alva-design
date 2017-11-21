19212906d606d02a0e7a603fc70585195f81a756
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2017-11-21 16:20:45 +0100

    feat: colors to string

--- patterns/atoms/button/index.tsx
+++ patterns/atoms/button/index.tsx
@@ -8,16 +8,16 @@ export interface ButtonProps {
 	order?: Order;
 
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  onMouseDown?: React.MouseEventHandler<HTMLButtonElement>;
- }
+	onMouseDown?: React.MouseEventHandler<HTMLButtonElement>;
+}
 
 export enum Order {
 	Primary,
 	Secondary
- }
+}
 
 const StyledButton = styled.button`
- 	padding: 15px 30px;
+	padding: 15px 30px;
 	min-width: 200px;
 	font-size: 18px;
 	font-family: ${fonts().NORMAL_FONT};
@@ -26,31 +26,31 @@ const StyledButton = styled.button`
 		switch (props.order) {
 			case Order.Secondary:
 				return css`
-					background: ${colors.white};
-					border: 1px solid ${colors.green};
-					color: ${colors.green};
+					background: ${colors.white.toString()};
+					border: 1px solid ${colors.green.toString()};
+					color: ${colors.green.toString()};
 					&:hover {
-						border-color: ${colors.greenLight};
-						color: ${colors.greenLight};
+						border-color: ${colors.greenLight.toString()};
+						color: ${colors.greenLight.toString()};
 					}
 					&:disabled {
-						border-color: ${colors.grey70};
-						color: ${colors.grey70};
+						border-color: ${colors.grey70.toString()};
+						color: ${colors.grey70.toString()};
 					}
 				`
 		  case Order.Primary:
 		  default:
 				return css`
-					background: ${colors.green};
-					border: 1px solid ${colors.greenDark};
-					color: ${colors.white};
+					background: ${colors.green.toString()};
+					border: 1px solid ${colors.greenDark.toString()};
+					color: ${colors.white.toString()};
 					&:hover {
-						background-color: ${colors.greenLight};
-						border-color: ${colors.green};
+						background-color: ${colors.greenLight.toString()};
+						border-color: ${colors.green.toString()};
 					}
 					&:disabled {
-						border-color: ${colors.grey70};
-						background-color: ${colors.grey70};
+						border-color: ${colors.grey70.toString()};
+						background-color: ${colors.grey70.toString()};
 					}
 				`
 			}
@@ -60,8 +60,8 @@ const StyledButton = styled.button`
 		? css`
 				cursor: pointer;
 			`
-    : ''
-  }
+		: ''
+	}
 `;
 
 const Button: React.StatelessComponent<ButtonProps> = (props): JSX.Element => {
@@ -69,8 +69,8 @@ const Button: React.StatelessComponent<ButtonProps> = (props): JSX.Element => {
 		<StyledButton
 			order={props.order}
 			disabled={Boolean(props.disabled)}
-      onClick={props.onClick}
-      onMouseDown={props.onMouseDown}
+			onClick={props.onClick}
+			onMouseDown={props.onMouseDown}
 			>
 			{props.children}
 		</StyledButton>
