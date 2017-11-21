c3cd9eaea7571db414180950fd1826d1cb501905
Lasse Küchler <lasse.kuechler@sinnerschrader.com>
2017-11-21 16:25:51 +0100

    add layout pattern

---
+++ patterns/atoms/layout/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+
+export interface LayoutProps {
+	direction?: 'horizontal' | 'vertical';
+}
+
+const StyledLayout = styled.div`
+	display: flex;
+
+	${(props: LayoutProps) => {
+		switch (props.direction) {
+			case 'horizontal':
+				return css`
+					flex-direction: row;
+				`
+			case 'vertical':
+			default:
+				return css`
+					flex-direction: column;
+				`
+		}
+	}}
+`;
+
+const Layout: React.StatelessComponent<LayoutProps> = (props) => {
+	return (
+		<StyledLayout>
+			{props.children}
+		</StyledLayout>
+	);
+};
+
+export default Layout;
