6539ad3315e727a11a85ae052a2f11dfeee1767b
Markus Oelhafen <oelhafenmarkus@gmail.com>
2017-11-21 11:49:32 +0100

    add multiple headline sizes

---
+++ patterns/atoms/headline/demo.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Headline from 'Pattern';
+
+const HeadlineDemo: React.StatelessComponent<void> = (): JSX.Element => {
+	return (
+		<div>
+			<Headline order={1} tagName="h1">
+				Headline Order 1
+			</Headline>
+			<Headline order={2}>
+				Headline Order 2
+			</Headline>
+			<Headline order={3}>
+				Headline Order 3
+			</Headline>
+		</div>
+	);
+};
+
+export default HeadlineDemo;
