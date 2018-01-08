f9736b9f1fc88bbb175bedeb3b390b8064c60751
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2018-01-08 11:46:29 +0100

    feat(headline): add uppercase property

--- patterns/atoms/headline/demo.tsx
+++ patterns/atoms/headline/demo.tsx
@@ -7,6 +7,9 @@ const HeadlineDemo: React.StatelessComponent<void> = (): JSX.Element => {
 			<Headline level={Level.H1}>Headline Order 1</Headline>
 			<Headline level={Level.H2}>Headline Order 2</Headline>
 			<Headline level={Level.H3}>Headline Order 3</Headline>
+			<Headline level={Level.H1} uppercase>
+				Headline Order 1
+			</Headline>
 			<Headline level={Level.H3} textAlign={TextAlign.Center}>
 				Headline Order 3
 			</Headline>
