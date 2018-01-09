d061687d73a9091ada096f8abaf7331c88e78528
Alexander Peschel <alexander.peschel@sinnerschrader.com>
2018-01-09 13:36:34 +0100

    feat: introduce input component

---
+++ patterns/atoms/input/demo.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Input from './index';
+import Space, { Size as SpaceSize } from '../space';
+
+const InputDemo: React.StatelessComponent<void> = (): JSX.Element => {
+	return (
+		<div>
+			<Input labelText="Label" placeholder="Placeholder" />
+			<Space size={SpaceSize.M} />
+			<Input labelText="Label" value="The quick brown fox jumps over the lazy dog" />
+			<Space size={SpaceSize.M} />
+			<Input
+				labelText="Label"
+				value="The quick brown fox jumps over the lazy dog"
+				errorText="Something went wrong :("
+			/>
+		</div>
+	);
+};
+
+export default InputDemo;
