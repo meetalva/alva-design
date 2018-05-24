393c83508bb34269110c86ec7a018d0d7b765528
Mario Nebl <hello@mario-nebl.de>
2018-05-24 17:54:56 +0200

    chore: update to latest patternplate

--- patternplate.config.js
+++ patternplate.config.js
@@ -2,8 +2,5 @@ module.exports = {
   docs: ["docs/**/*.md"],
   entry: ["lib/patterns/**/demo.js"],
   render: "@patternplate/render-styled-components/render",
-  mount: "@patternplate/render-styled-components/mount",
-  ui: {
-    title: "@patternplate/components"
-  }
+  mount: "@patternplate/render-styled-components/mount"
 };
