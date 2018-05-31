67d90d2978a607c11f22f3f295084734de41ad12
Mario Nebl <hello@mario-nebl.de>
2018-05-31 17:49:22 +0200

    refactor: patterns -> src

--- patternplate.config.js
+++ patternplate.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   docs: ["docs/**/*.md"],
-  entry: ["lib/patterns/**/demo.js"],
+  entry: ["lib/**/demo.js"],
   render: "@patternplate/render-styled-components/render",
   mount: "@patternplate/render-styled-components/mount"
 };
