5686f5e8ec796a3eadd85da658889c4f28d7d91f
Tilman Frick <git@tilman.io>
2018-06-06 11:10:44 +0200

    feat: add slots to teaser component

--- src/molecules/teaser/index.tsx
+++ src/molecules/teaser/index.tsx
@@ -1,29 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
-import Button from "../../atoms/button";
 import colors from "../../atoms/colors";
-import Copy, { Size } from "../../atoms/copy";
-import Headline from "../../atoms/headline";
 
 export interface TeaserProps {
-	/** @name Button label @default Label */ buttonLabel: string;
-	/** @name Copy text @default Copytext */ copyText: string;
-	/** @name Headline level @default H1 */ headlineLevel: Level;
-	/** @name Headline align @default Left */ headlineAlign?: TextAlign;
-	/** @name Headline text @default Headline */ headlineText: string;
+	/** @name Headline text @default Headline */ headline: React.ReactNode;
+	/** @name Button label @default Button */ button: React.ReactNode;
 	/** @ignore */ onClick?: React.MouseEventHandler<HTMLButtonElement>;
-}
-
-export enum Level {
-	H1,
-	H2,
-	H3
-}
-
-export enum TextAlign {
-	Left,
-	Center,
-	Right
 }
 
 const StyledTeaser = styled.div`
@@ -32,22 +14,11 @@ const StyledTeaser = styled.div`
 	border: 1px solid ${colors.grey90.toString()};
 `;
 
-const StyledHeadline = styled(Headline)`
-	margin-bottom: 30px;
-`;
-
-const StyledCopy = styled(Copy)`
-	margin-bottom: 30px;
-`;
-
 const Teaser: React.StatelessComponent<TeaserProps> = (props): JSX.Element => {
 	return (
 		<StyledTeaser>
-			<StyledHeadline level={props.headlineLevel}>{props.headlineText}</StyledHeadline>
-			<StyledCopy size={Size.Medium}>{props.copyText}</StyledCopy>
-			<Button onClick={props.onClick} primary={true}>
-				{props.buttonLabel}
-			</Button>
+			{props.headline}
+			{props.button}
 		</StyledTeaser>
 	);
 };
