0908d19598255898b5f5bf43a9d08ee13b7236e8
Markus Ölhafen <markus.oelhafen@sinnerschrader.com>
2018-06-17 18:06:46 +0200

    feat(components): introduce menu-element

---
+++ src/molecules/menu-element/demo.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import MenuElement from "./index";
+
+export default function MenuElementDemo() {
+	return (
+		<MenuElement />
+	);
+}
---
+++ src/molecules/menu-element/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import styled from "styled-components";
+import colors from "../../atoms/colors";
+import Copy, {Size} from "../../atoms/copy";
+
+export interface HeaderProps {
+	/** @name Copy */ linkName?: string;
+	/** @name Link */ onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	/** @ignore */ onMouseDown?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const StyledMenuItem = styled(Copy)`
+	color: ${colors.white.toString()};
+	margin-left: 50px;
+	&:first-of-type {
+		margin: 0;
+	}
+`;
+
+const Menu: React.StatelessComponent<HeaderProps> = (props): JSX.Element => {
+	return (
+		<StyledMenuItem size={Size.Small}>
+			{props.linkName}
+		</StyledMenuItem>
+	);
+};
+
+export default Menu;
