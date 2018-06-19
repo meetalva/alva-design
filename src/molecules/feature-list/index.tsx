44a9949ba33f6b8cdb27711fc03fa7cdb17f012c
Markus Ölhafen <markus.oelhafen@sinnerschrader.com>
2018-06-19 11:51:13 +0200

    fix: remove unneeded styling

--- src/molecules/feature-list/index.tsx
+++ src/molecules/feature-list/index.tsx
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const StyledFeatureList = styled.div`
 	max-width: 1080px;
 	margin: 0 auto;
-	vertical-align: top;
 `;
+
 
 const FeatureList: React.StatelessComponent = (props): JSX.Element => {
 	return (
