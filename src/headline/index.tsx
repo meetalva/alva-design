c6b57a8b742a33c43cc5023f0c0032eafb87b17c
Markus Ölhafen <markus.oelhafen@sinnerschrader.com>
2018-06-22 19:12:06 +0200

    fix: make all sizes responsive

--- src/headline/index.tsx
+++ src/headline/index.tsx
@@ -28,24 +28,40 @@ const StyledHeadline = styled.div`
 	font-family: ${fonts().NORMAL_FONT};
 	font-weight: 500;
 	color: ${(props: HeadlineProps) => props.color || 'inherit'};
+	line-height: 1.3;
 
 	${(props: HeadlineProps) => {
 		switch (props.level) {
 			case Level.H3:
 				return css`
-					font-size: 18px;
-					line-height: 24px;
+					font-size: 24px;
+
+					@media screen and (min-width: 960px) {
+						font-size: 32px;
+					}
 				`;
 			case Level.H2:
 				return css`
-					font-size: 42px;
-					line-height: 48px;
+					font-size: 32px;
+
+					@media screen and (min-width: 450px) {
+						font-size: 48px;
+					}
+					@media screen and (min-width: 960px) {
+						font-size: 54px;
+					}
 				`;
 			case Level.H1:
 			default:
 				return css`
-					font-size: 96px;
-					line-height: 132px;
+					font-size: 48px;
+
+					@media screen and (min-width: 450px) {
+						font-size: 64px;
+					}
+					@media screen and (min-width: 960px) {
+						font-size: 96px;
+					}
 				`;
 		}
 	}};
