dd7bdf28affef2f947b903aa10bacc9776714384
Tilman Frick <git@tilman.io>
2018-09-16 16:24:27 +0200

    fix: rename navigation to menu

--- src/menu/demo.tsx
+++ src/menu/demo.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import Navigation from './index';
+import Menu from './index';
 import MenuItem from "../menu-item";
 
-const NavigationDemo: React.StatelessComponent<void> = (): JSX.Element => {
+const MenuDemo: React.StatelessComponent<void> = (): JSX.Element => {
 	return (
-		<Navigation sticky logo="/api/static/alva-small.png">
+		<Menu sticky logo="/api/static/alva-small.png">
 			<MenuItem linkName="Story" />
 			<MenuItem linkName="Learn" />
 			<MenuItem linkName="API" />
 			<MenuItem linkName="Github" />
 			<MenuItem linkName="Twitter" />
-		</Navigation>
+		</Menu>
 	);
 };
 
-export default NavigationDemo;
+export default MenuDemo;
