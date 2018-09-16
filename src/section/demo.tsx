89af5b6f946d883339b43485cbd0cbfe15679e77
Tilman Frick <git@tilman.io>
2018-09-16 18:50:29 +0200

    feat: add section component

---
+++ src/section/demo.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Section from './index';
+import Headline, {Level } from '../headline';
+import Copy, { Size as CopySize } from '../copy';
+import Space, { Size as SpaceSize} from '../space';
+import { Color } from '../colors';
+import Button, { ButtonOrder } from '../button';
+import Layout from '../layout';
+
+const SectionDemo: React.StatelessComponent<void> = (): JSX.Element => {
+	return (
+		<Section backgroundColor={Color.Black} textColor={Color.White}>
+			<Headline level={Level.H1}>
+				Create <u>living prototypes</u> with code components.
+			</Headline>
+			<Space size={SpaceSize.M} />
+			<Layout maxWidth="640px">
+				<Copy size={CopySize.Large}>
+					Alva lets you to design interactive products based on the same components your developers are using for production websites. And guess what – we are entirely open source.
+				</Copy>
+			</Layout>
+			<Space size={SpaceSize.L} />
+			<Button order={ButtonOrder.Primary} onClick={() => alert('Click!')}>
+				Download Beta for macOS
+			</Button>
+			<Space size={SpaceSize.S} />
+			<Copy color={Color.Grey70} size={CopySize.Small}>Also available for <u>Windows</u> and <u>Linux</u></Copy>
+		</Section>
+	);
+};
+
+export default SectionDemo;
