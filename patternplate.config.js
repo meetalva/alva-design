9a80e5bceb95c657f73695a989cdcb4d91624a2e
Tilman Frick <git@tilman.io>
2018-09-17 14:31:02 +0200

    feat: use patternplate cover as website

--- patternplate.config.js
+++ patternplate.config.js
@@ -2,5 +2,6 @@ module.exports = {
   docs: ["docs/**/*.md"],
   entry: ["lib/**/demo.js"],
   render: "@patternplate/render-styled-components/render",
-  mount: "@patternplate/render-styled-components/mount"
+  mount: "@patternplate/render-styled-components/mount",
+  cover: "./lib/01-page/demo.js"
 };
